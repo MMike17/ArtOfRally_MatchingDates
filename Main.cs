e7b83e32e1298e576f675db5e8141eee7214908a
Mike <matthews.r.mike@gmail.com>
2024-09-02 22:45:32 +0200

    Add settings

--- Main.cs
+++ Main.cs
@@ -2,6 +2,7 @@
 using RealCarNames;
 using System;
 using System.Reflection;
+using UnityModManagerNet;
 
 using static UnityModManagerNet.UnityModManager;
 
@@ -10,13 +11,18 @@ namespace MatchingDates
     static class Main
     {
         public static bool enabled { get; private set; }
+        public static Settings settings { get; private set; }
 
         static ModEntry.ModLogger logger;
 
         static bool Load(ModEntry modEntry)
         {
             logger = modEntry.Logger;
+            settings = ModSettings.Load<Settings>(modEntry);
+
             modEntry.OnToggle = OnToggle;
+            modEntry.OnGUI = entry => settings.Draw(modEntry);
+            modEntry.OnSaveGUI = entry => settings.Save(modEntry);
 
             Harmony harmony = new Harmony(modEntry.Info.Id);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
@@ -28,6 +34,15 @@ namespace MatchingDates
         {
             enabled = state;
             return true;
+        }
+
+        public static void RefreshCarLocks()
+        {
+            // what are the original car locks ?
+            // what are the new car locks ?
+
+            // set unlock date
+            // make sure the car is unlocked
         }
 
         public static bool IsCarValid(string carName, int rallyYear) => rallyYear >= CarNameProvider.GetCarYear(carName);
---
+++ Settings.cs
@@ -0,0 +1,24 @@
+﻿using UnityModManagerNet;
+
+using static UnityModManagerNet.UnityModManager;
+
+namespace MatchingDates
+{
+    public class Settings : ModSettings, IDrawable
+    {
+        public enum Mode
+        {
+            lock_to_date
+        }
+
+        [Draw(DrawType.PopupList)]
+        public Mode mode;
+
+        public void OnChange()
+        {
+            Main.RefreshCarLocks();
+        }
+
+        public override void Save(ModEntry modEntry) => Save(this, modEntry);
+    }
+}
