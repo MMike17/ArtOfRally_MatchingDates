b9d33998255412254f40a00e1685dbafd891313a
Mike <matthews.r.mike@gmail.com>
2024-09-02 18:48:57 +0200

    Add non-blocking structure to patches

--- Patches.cs
+++ Patches.cs
@@ -6,6 +6,27 @@ using System.Reflection;
 
 namespace MatchingDates
 {
+    // /!\ Doesn't work when we move between seasons... /!\
+    // detect when we leave the screen (forward or backwards)
+    // reset all data (mainly detectedYear)
+
+    // /!\ Methods don't get called when we go back to the panel... /!\
+    // Can I highjack what is going from panel to panel ?
+
+    // DOESN'T WORK
+
+    // CarChooserHelper.BeginEvent
+    // CarChooserManager.UnInitForCarChooser
+
+    //[HarmonyPatch(typeof(), nameof())]
+    //static class x_x_Patch
+    //{
+    //    static void Postfix()
+    //    {
+    //        Main.Log("Post fix for \" \"");
+    //    }
+    //}
+
     [HarmonyPatch(typeof(CarChooserHelper), "Init")]
     static class CarChooserHelper_Init_Patch
     {
@@ -19,8 +40,11 @@ namespace MatchingDates
             if (!Main.enabled || GameModeManager.GameMode != GameModeManager.GAME_MODES.CAREER)
                 return;
 
-            // reset car selection index
-            SaveGame.SetInt(GameModeManager.GetSeasonDataCurrentGameMode().CarClass.ToString(), 0);
+            Main.Try(() =>
+            {
+                // reset car selection index
+                SaveGame.SetInt(GameModeManager.GetSeasonDataCurrentGameMode().CarClass.ToString(), 0);
+            });
         }
 
         static void Postfix(CarChooserHelper __instance)
@@ -28,47 +52,50 @@ namespace MatchingDates
             if (!Main.enabled || GameModeManager.GameMode != GameModeManager.GAME_MODES.CAREER)
                 return;
 
-            // remove unavailable cars from selection list
-            originalList = new List<string>(__instance.CarButton.stringList);
-            truncatedList = new List<string>(__instance.CarButton.stringList);
-
-            detectedYear = int.Parse(__instance.GroupTitle.Text.text.Split(new string[] { "  |  " }, StringSplitOptions.None)[1]);
-            List<string> toRemove = new List<string>();
-
-            originalList.ForEach(carName =>
+            Main.Try(() =>
             {
-                if (!Main.IsCarValid(carName, detectedYear))
-                    toRemove.Add(carName);
+                // remove unavailable cars from selection list
+                originalList = new List<string>(__instance.CarButton.stringList);
+                truncatedList = new List<string>(__instance.CarButton.stringList);
+
+                detectedYear = int.Parse(__instance.GroupTitle.Text.text.Split(new string[] { "  |  " }, StringSplitOptions.None)[1]);
+                List<string> toRemove = new List<string>();
+
+                originalList.ForEach(carName =>
+                {
+                    if (!Main.IsCarValid(carName, detectedYear))
+                        toRemove.Add(carName);
+                });
+
+                string debug = string.Empty;
+                toRemove.ForEach(carName =>
+                {
+                    truncatedList.Remove(carName);
+                    debug += "\n- " + carName + " (" + CarNameProvider.GetCarYear(carName) + ")";
+                });
+                Main.Log("Removed cars :" + debug);
+
+                if (truncatedList.Count == 0)
+                {
+                    truncatedList = originalList;
+                    detectedYear = 0;
+                    Main.Log("New list is empty. Disabling feature until next call.");
+                    return;
+                }
+
+                // set list of cars for selection
+                __instance.CarButton.stringList = truncatedList;
+                __instance.CarButton.stringListLength = truncatedList.Count - 1;
+
+                // updated UI stats and name
+                __instance.CarButton.GetType().GetMethod("UpdateCarSpecs", BindingFlags.NonPublic | BindingFlags.Instance).
+                    Invoke(__instance.CarButton, null);
+                __instance.CarButton.UpdateOptionTextAndArrows();
+
+                // reselect car model
+                UIManager.Instance.PanelManager.CarChooserManager.SelectCarInClass(GameModeManager.GetSeasonDataCurrentGameMode().CarClass, 0);
+                Main.Log("Reselected car " + truncatedList[0]);
             });
-
-            string debug = string.Empty;
-            toRemove.ForEach(carName =>
-            {
-                truncatedList.Remove(carName);
-                debug += "\n- " + carName + " (" + CarNameProvider.years[CarNameProvider.DetectCarName(carName)] + ")";
-            });
-            Main.Log("Removed cars :" + debug);
-
-            if (truncatedList.Count == 0)
-            {
-                truncatedList = originalList;
-                detectedYear = 0;
-                Main.Log("New list is empty. Disabling feature until next call.");
-                return;
-            }
-
-            // set list of cars for selection
-            __instance.CarButton.stringList = truncatedList;
-            __instance.CarButton.stringListLength = truncatedList.Count - 1;
-
-            // updated UI stats and name
-            __instance.CarButton.GetType().GetMethod("UpdateCarSpecs", BindingFlags.NonPublic | BindingFlags.Instance).
-                Invoke(__instance.CarButton, null);
-            __instance.CarButton.UpdateOptionTextAndArrows();
-
-            // reselect car model
-            UIManager.Instance.PanelManager.CarChooserManager.SelectCarInClass(GameModeManager.GetSeasonDataCurrentGameMode().CarClass, 0);
-            Main.Log("Reselected car " + truncatedList[0]);
         }
 
         public static int AdjustedToNormalIndex(int carIndex)
@@ -78,6 +105,8 @@ namespace MatchingDates
 
             return originalList.IndexOf(truncatedList[carIndex]);
         }
+
+        public static void Reset() => detectedYear = 0;
     }
 
     [HarmonyPatch(typeof(CarManager), nameof(CarManager.GetCurrentCarsListForClass))]
@@ -87,16 +116,22 @@ namespace MatchingDates
         {
             if (!Main.enabled || GameModeManager.GameMode != GameModeManager.GAME_MODES.CAREER || CarChooserHelper_Init_Patch.detectedYear == 0)
                 return;
+            List<Car> result = __result;
 
-            List<Car> toRemove = new List<Car>();
-            __result.ForEach(car =>
+            Main.Try(() =>
             {
-                if (!Main.IsCarValid(car.name, CarChooserHelper_Init_Patch.detectedYear))
-                    toRemove.Add(car);
+                List<Car> toRemove = new List<Car>();
+                result.ForEach(car =>
+                {
+                    if (!Main.IsCarValid(car.name, CarChooserHelper_Init_Patch.detectedYear))
+                        toRemove.Add(car);
+                });
+
+                foreach (Car car in toRemove)
+                    result.Remove(car);
             });
 
-            foreach (Car car in toRemove)
-                __result.Remove(car);
+            __result = result;
         }
     }
 
@@ -109,11 +144,14 @@ namespace MatchingDates
             if (!Main.enabled || GameModeManager.GameMode != GameModeManager.GAME_MODES.CAREER || CarChooserHelper_Init_Patch.detectedYear == 0)
                 return;
 
-            index = CarChooserHelper_Init_Patch.AdjustedToNormalIndex(index);
+            int newIndex = index;
+
+            Main.Try(() =>
+            {
+                newIndex = CarChooserHelper_Init_Patch.AdjustedToNormalIndex(newIndex);
+            });
+
+            index = newIndex;
         }
     }
-
-    // Doesn't work when we move between seasons...
-    // detect when we leave the screen (forward or backwards)
-    // reset all data (mainly detectedYear)
 }
